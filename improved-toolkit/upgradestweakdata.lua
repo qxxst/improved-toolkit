f9ae371e6b6a758577d4b23a7d71da974fa8628b
questmatrix <31394614+questmatrix@users.noreply.github.com>
2023-11-01 14:48:22 -0500

    Updated upgradestweakdata.lua

--- improved-toolkit/upgradestweakdata.lua
+++ improved-toolkit/upgradestweakdata.lua
@@ -2,5 +2,8 @@ local module = ... or D:module('improved_toolkit')
 local UpgradesTweakData = module:hook_class("UpgradesTweakData")
 
 module:post_hook(UpgradesTweakData, "_player_definitions", function(self)
-	-- placeholder
+	self.values.player.toolset[1] = 0.825
+	self.values.player.toolset[2] = 0.75
+	self.values.player.toolset[3] = 0.625
+	self.values.player.toolset[4] = 0.5
 end, true)
